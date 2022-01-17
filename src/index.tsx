9a753977f6a5d4f4071ba6dacd4b6df4bab2a24d
Flavio deSouza <flavio.desouza@alphasights.com>
2022-01-17 12:07:26 -0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Helllo</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
