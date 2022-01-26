5821c23f5e593545b04450fd79ec274c624db195
Flavio deSouza <flavio.desouza@alphasights.com>
2022-01-25 21:05:08 -0300

    using hash route for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ReviewsList from "./pages/ReviewsList";
 import ReviewPage from "./pages/ReviewPage";
 import AboutPage from "./pages/AboutPage";
 import Error404 from "./pages/Error404";
-import { NavBar } from "./components/NavBar"
+import { NavBar } from "./components/NavBar";
 import { ReviewService } from "./domain/ReviewService";
 import { ReviewServiceJson } from "./infrastructure/ReviewServiceJson";
 
 const service: ReviewService = ReviewServiceJson;
 
 export const App: React.FC = (props) => {
-   return (
+  return (
     <>
-      <Router basename="/my-blog">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/review" element={<ReviewsList reviewService={service} />} />
-          <Route path="/review/:name" element={<ReviewPage reviewService={service} />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-      </Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/review"
+          element={<ReviewsList reviewService={service} />}
+        />
+        <Route
+          path="/review/:name"
+          element={<ReviewPage reviewService={service} />}
+        />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </>
   );
 };
